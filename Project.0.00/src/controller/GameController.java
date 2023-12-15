63fa6337e7908845f254c12d62b439bfa222c3da
JIYU-qiu <153187405+JIYU-qiu@users.noreply.github.com>
2023-12-15 23:14:55 +0800

    Add files via upload
    damo-23 无修改

---
+++ Project.0.00/src/controller/GameController.java
@@ -0,0 +1,151 @@
+package controller;
+
+import listener.GameListener;
+import model.Constant;
+import model.Chessboard;
+import model.ChessboardPoint;
+import view.CellComponent;
+import view.ChessComponent;
+import view.ChessGameFrame;
+import view.ChessboardComponent;
+
+import javax.swing.*;
+
+/**
+ * Controller is the connection between model and view,
+ * when a Controller receive a request from a view, the Controller
+ * analyzes and then hands over to the model for processing
+ * [in this demo the request methods are onPlayerClickCell() and
+ * onPlayerClickChessPiece()]
+ */
+public class GameController implements GameListener {
+
+    private Chessboard model;
+    private ChessboardComponent view;
+
+
+    // Record whether there is a selected piece before
+    private ChessboardPoint selectedPoint;
+    private ChessboardPoint selectedPoint2;
+
+    private int score;
+
+    private JLabel statusLabel;
+
+    public JLabel getStatusLabel() {
+        return statusLabel;
+    }
+
+    public void setStatusLabel(JLabel statusLabel) {
+        this.statusLabel = statusLabel;
+    }
+
+    public GameController(ChessboardComponent view, Chessboard model) {
+        this.view = view;
+        this.model = model;
+
+        view.registerController(this);
+        view.initiateChessComponent(model);
+        view.repaint();
+    }
+
+    public void initialize() {
+        for (int i = 0; i < Constant.CHESSBOARD_ROW_SIZE.getNum(); i++) {
+            for (int j = 0; j < Constant.CHESSBOARD_COL_SIZE.getNum(); j++) {
+                //todo: complete it when restart game
+            }
+        }
+    }
+
+    // click an empty cell
+    @Override
+    public void onPlayerClickCell(ChessboardPoint point, CellComponent component) {
+    }
+
+    @Override
+    public void onPlayerSwapChess() {
+        // TODO: Init your swap function here.
+        System.out.println("Implement your swap here.");
+    }
+
+    @Override
+    public void onPlayerNextStep() {
+        // TODO: Init your next step function here.
+        System.out.println("Implement your next step here.");
+        score++;
+        this.statusLabel.setText("Score:" + score);
+
+    }
+
+    // click a cell with a chess
+    @Override
+    public void onPlayerClickChessPiece(ChessboardPoint point, ChessComponent component) {
+        if (selectedPoint2 != null) {
+            var distance2point1 = Math.abs(selectedPoint.getCol() - point.getCol()) + Math.abs(selectedPoint.getRow() - point.getRow());
+            var distance2point2 = Math.abs(selectedPoint2.getCol() - point.getCol()) + Math.abs(selectedPoint2.getRow() - point.getRow());
+            var point1 = (ChessComponent) view.getGridComponentAt(selectedPoint).getComponent(0);
+            var point2 = (ChessComponent) view.getGridComponentAt(selectedPoint2).getComponent(0);
+            if (distance2point1 == 0 && point1 != null) {
+                point1.setSelected(false);
+                point1.repaint();
+                selectedPoint = selectedPoint2;
+                selectedPoint2 = null;
+            } else if (distance2point2 == 0 && point2 != null) {
+                point2.setSelected(false);
+                point2.repaint();
+                selectedPoint2 = null;
+            } else if (distance2point1 == 1 && point2 != null) {
+                point2.setSelected(false);
+                point2.repaint();
+                selectedPoint2 = point;
+                component.setSelected(true);
+                component.repaint();
+            } else if (distance2point2 == 1 && point1 != null) {
+                point1.setSelected(false);
+                point1.repaint();
+                selectedPoint = selectedPoint2;
+                selectedPoint2 = point;
+                component.setSelected(true);
+                component.repaint();
+            }
+            return;
+        }
+
+
+        if (selectedPoint == null) {
+            selectedPoint = point;
+            component.setSelected(true);
+            component.repaint();
+            return;
+        }
+
+        var distance2point1 = Math.abs(selectedPoint.getCol() - point.getCol()) + Math.abs(selectedPoint.getRow() - point.getRow());
+
+        if (distance2point1 == 0) {
+            selectedPoint = null;
+            component.setSelected(false);
+            component.repaint();
+            return;
+        }
+
+        if (distance2point1 == 1) {
+            selectedPoint2 = point;
+            component.setSelected(true);
+            component.repaint();
+        } else {
+            selectedPoint2 = null;
+
+            var grid = (ChessComponent) view.getGridComponentAt(selectedPoint).getComponent(0);
+            if (grid == null) return;
+            grid.setSelected(false);
+            grid.repaint();
+
+            selectedPoint = point;
+            component.setSelected(true);
+            component.repaint();
+        }
+
+
+    }
+
+}
---
+++ Project.0.00/src/model/Chessboard.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.Random;
+
+/**
+ * This class store the real chess information.
+ * The Chessboard has 8 * 8 cells, and each cell has a position for chess
+ */
+public class Chessboard {
+    private Cell[][] grid;
+
+    public Chessboard() {
+        this.grid =
+                new Cell[Constant.CHESSBOARD_ROW_SIZE.getNum()][Constant.CHESSBOARD_COL_SIZE.getNum()];
+
+        initGrid();
+        initPieces();
+    }
+
+    private void initGrid() {
+        for (int i = 0; i < Constant.CHESSBOARD_ROW_SIZE.getNum(); i++) {
+            for (int j = 0; j < Constant.CHESSBOARD_COL_SIZE.getNum(); j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+    }
+
+    private void initPieces() {
+
+        for (int i = 0; i < Constant.CHESSBOARD_ROW_SIZE.getNum(); i++) {
+            for (int j = 0; j < Constant.CHESSBOARD_COL_SIZE.getNum(); j++) {
+                grid[i][j].setPiece(new ChessPiece( Util.RandomPick(new String[]{"💎", "⚪", "▲", "🔶"})));
+            }
+        }
+
+    }
+
+    public ChessPiece getChessPieceAt(ChessboardPoint point) {
+        return getGridAt(point).getPiece();
+    }
+
+    public Cell getGridAt(ChessboardPoint point) {
+        return grid[point.getRow()][point.getCol()];
+    }
+
+    private int calculateDistance(ChessboardPoint src, ChessboardPoint dest) {
+        return Math.abs(src.getRow() - dest.getRow()) + Math.abs(src.getCol() - dest.getCol());
+    }
+
+    public ChessPiece removeChessPiece(ChessboardPoint point) {
+        ChessPiece chessPiece = getChessPieceAt(point);
+        getGridAt(point).removePiece();
+        return chessPiece;
+    }
+
+    public void setChessPiece(ChessboardPoint point, ChessPiece chessPiece) {
+        getGridAt(point).setPiece(chessPiece);
+    }
+
+
+    public void swapChessPiece(ChessboardPoint point1, ChessboardPoint point2) {
+        var p1 = getChessPieceAt(point1);
+        var p2 = getChessPieceAt(point2);
+        setChessPiece(point1, p2);
+        setChessPiece(point2, p1);
+    }
+
+
+    public Cell[][] getGrid() {
+        return grid;
+    }
+
+
+
+}
